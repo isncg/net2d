77318fcd92f3f7e225453b54a9ba707ccb76a4dd
isncg <isn_cg@163.com>
2019-05-18 10:37:02 +0800

    sub window

--- main.cpp
+++ main.cpp
@@ -5,23 +5,30 @@
 #include<chrono>
 
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK RenderProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+void ResizeRenderingArea();
+
 HWND hWnd;
+HWND hMainWnd;
 
-
-ATOM RegisterMainWindow(const char * name, HINSTANCE hInstance);
+ATOM RegisterWindowClasses(HINSTANCE hInstance);
 
 Model* model;
 View* view;
 Controller* controller;
 
 
-auto wind_class_name = "N2D_WINDOW_CLASS";
+auto main_window_class_name = "N2D_WINDOW_CLASS";
+auto render_window_class_name = "N2D_RENDERING_CLASS";
+
 auto wind_tittle = "N2D";
 
 #include<iostream>
 HDC backdc = NULL;
 HBITMAP backbuffer = NULL;
 
+
+void Render(HDC hdc);
 
 int main() {
 	RegisterNetMessage();
@@ -37,9 +44,9 @@ int main() {
 
 	std::list <POINT_MESSAGE> saved;
 	db.GetData(saved);
-	
+
 	for (auto i = saved.begin(); i != saved.end(); i++) {
-		printf("SAVED: x=%f y=%f\n", i->x, i->y, i->size=10);
+		printf("SAVED: x=%f y=%f\n", i->x, i->y, i->size = 10);
 	}
 
 
@@ -58,24 +65,29 @@ int main() {
 	HINSTANCE hInstance = GetModuleHandle(NULL);
 
 
-	if (!RegisterMainWindow(wind_class_name, hInstance)) {
+	if (!RegisterWindowClasses(hInstance)) {
 		return -1;
 	}
 
 
 	RECT rc = { 0, 0, 640, 480 };
 	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
-	hWnd = CreateWindow(wind_class_name, wind_tittle, WS_OVERLAPPEDWINDOW,
+	hMainWnd = CreateWindow(main_window_class_name, wind_tittle, WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance,
 		NULL);
-	if (!hWnd)
+	if (!hMainWnd)
 	{
 		return -1;
 	}	//return E_FAIL;
 
 
-	ShowWindow(hWnd, SW_SHOW);
+	ShowWindow(hMainWnd, SW_SHOW);
 
+
+	hWnd = CreateWindow(render_window_class_name, "DRAW", WS_CHILD, 0, 0, 200, 300, hMainWnd, 0, hInstance, 0);
+	ShowWindow(hWnd, SW_SHOW);
+	ResizeRenderingArea();
+	SetWindowLong(hWnd, GWL_WNDPROC, (LONG)RenderProc);
 
 	LINE_MESSAGE testline;
 	testline.p0.x = 0;
@@ -103,58 +115,136 @@ int main() {
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
-		else
-		{
-			HDC hdc = GetDC(hWnd);
-			if (!backdc) {
-				backdc = CreateCompatibleDC(hdc);
-				if (backbuffer)
-					DeleteObject(backbuffer);
-				backbuffer = CreateCompatibleBitmap(hdc, 4096, 4096);
-				SelectObject(backdc, backbuffer);
-			}
+		/*	else
+			{*/
+			//HDC hdc = GetDCEx(hWnd, NULL, DCX_WINDOW);
+		HDC hdc = GetDC(hWnd);
+		if (!backdc) {
+			backdc = CreateCompatibleDC(hdc);
+			if (backbuffer)
+				DeleteObject(backbuffer);
+			backbuffer = CreateCompatibleBitmap(hdc, 4096, 4096);
+			SelectObject(backdc, backbuffer);
+		}
+
+		Render(hdc);
+
+		//if (backdc) {
+
+		//	RECT rc;
+		//	GetClientRect(hWnd, &rc);
+		//	FillRect(backdc, &rc, (HBRUSH)GetStockObject(WHITE_BRUSH));
+
+		//	view->Init(backdc);
+
+		//	view->DrawBackground(model);
+		//	view->PaintModel(model);
+		//	view->DrawForeground(model);
+		//	view->Cleanup();
+
+		//	auto width = rc.right - rc.left;
+		//	auto height = rc.bottom - rc.top;
+
+		//	/*	Gdiplus::Graphics graphics(backdc);
+		//		Gdiplus::Pen pen(Gdiplus::Color(255, 0, 0, 255));
+		//		graphics.DrawLine(&pen, 0, 0, 200, 300);*/
+		//	BitBlt(hdc, 0, 0, width, height, backdc, 0, 0, SRCCOPY);
+		///*	MoveToEx(hdc, 0, 0, 0);
+		//	LineTo(hdc, 200, 300);*/
+		//	//UpdateWindow(hSubWnd);
+		//}
+		ReleaseDC(hWnd, hdc);
+		auto exit_time = std::chrono::high_resolution_clock::now();
+		auto delta_time = exit_time - enter_time;
+
+		int sleep_time = 5 - delta_time.count() / 1000000;
+		if (sleep_time > 0) {
+			Sleep(sleep_time);
+		}
 
 
-			if (backdc) {
-
-				RECT rc;
-				GetClientRect(hWnd, &rc);
-				FillRect(backdc, &rc, (HBRUSH)GetStockObject(WHITE_BRUSH));
-
-				view->Init(backdc);
-			
-				view->DrawBackground(model);
-				view->PaintModel(model);
-				view->DrawForeground(model);
-				view->Cleanup();
-
-				auto width = rc.right - rc.left;
-				auto height = rc.bottom - rc.top;
-
-			/*	Gdiplus::Graphics graphics(backdc);
-				Gdiplus::Pen pen(Gdiplus::Color(255, 0, 0, 255));
-				graphics.DrawLine(&pen, 0, 0, 200, 300);*/
-				BitBlt(hdc, 0, 0, width, height, backdc, 0, 0, SRCCOPY);
-
-			}
-			ReleaseDC(hWnd, hdc);
-			auto exit_time = std::chrono::high_resolution_clock::now();
-			auto delta_time = exit_time - enter_time;
-
-			int sleep_time = 5 - delta_time.count() / 1000000;
-			if (sleep_time > 0) {
-				Sleep(sleep_time);
-			}
 		/*	exit_time = std::chrono::high_resolution_clock::now();
 			delta_time = exit_time - enter_time;*/
 			//printf("FPS: %.2lf  sleep: %d\n", 1000000000.0 / delta_time.count(), sleep_time);
-		}
+	//}
 
 	}
 
 
 	Gdiplus::GdiplusShutdown(gdiplusToken);
 	return 0;
+}
+
+
+void Render(HDC hdc) {
+	if (backdc) {
+
+		RECT rc;
+		GetClientRect(hWnd, &rc);
+		FillRect(backdc, &rc, (HBRUSH)GetStockObject(WHITE_BRUSH));
+
+		view->Init(backdc);
+
+		view->DrawBackground(model);
+		view->PaintModel(model);
+		view->DrawForeground(model);
+		view->Cleanup();
+
+		auto width = rc.right - rc.left;
+		auto height = rc.bottom - rc.top;
+
+		/*	Gdiplus::Graphics graphics(backdc);
+			Gdiplus::Pen pen(Gdiplus::Color(255, 0, 0, 255));
+			graphics.DrawLine(&pen, 0, 0, 200, 300);*/
+		BitBlt(hdc, 0, 0, width, height, backdc, 0, 0, SRCCOPY);
+		/*	MoveToEx(hdc, 0, 0, 0);
+			LineTo(hdc, 200, 300);*/
+			//UpdateWindow(hSubWnd);
+	}
+	/*ReleaseDC(hWnd, hdc);
+	auto exit_time = std::chrono::high_resolution_clock::now();
+	auto delta_time = exit_time - enter_time;
+
+	int sleep_time = 5 - delta_time.count() / 1000000;
+	if (sleep_time > 0) {
+		Sleep(sleep_time);
+	}*/
+}
+
+LRESULT CALLBACK RenderProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		Render(hdc);
+		EndPaint(hWnd, &ps);
+		break;
+
+	default:
+		if (controller->OnNetMessage(message, wParam, lParam)) {
+			InvalidateRect(hWnd, NULL, FALSE);
+			return 0;
+		}
+
+		if (controller->OnViewTransform(message, wParam, lParam)) {
+			InvalidateRect(hWnd, NULL, FALSE);
+			return 0;
+		}
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+
+}
+
+void ResizeRenderingArea() {
+	RECT rc;
+	GetClientRect(hMainWnd, &rc);
+	rc.top = 100;
+	SetWindowPos(::hWnd, NULL, rc.left, rc.top, rc.right - rc.left, rc.bottom - rc.top, 0);
 }
 
 
@@ -177,18 +267,22 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		PostQuitMessage(0);
 		break;
 
+	case WM_SIZE:
+		ResizeRenderingArea();
+		//InvalidateRect(::hWnd, NULL, FALSE);
+		break;
 
 	default:
 
-		if (controller->OnNetMessage(message, wParam, lParam)) {
-			InvalidateRect(hWnd, NULL, FALSE);
-			return 0;
-		}
+		/*	if (controller->OnNetMessage(message, wParam, lParam)) {
+				InvalidateRect(hWnd, NULL, FALSE);
+				return 0;
+			}
 
-		if (controller->OnViewTransform(message, wParam, lParam)) {
-			InvalidateRect(hWnd, NULL, FALSE);
-			return 0;
-		}
+			if (controller->OnViewTransform(message, wParam, lParam)) {
+				InvalidateRect(hWnd, NULL, FALSE);
+				return 0;
+			}*/
 
 		return DefWindowProc(hWnd, message, wParam, lParam);
 	}
@@ -196,9 +290,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-ATOM RegisterMainWindow(const char * name, HINSTANCE hInstance)
+ATOM RegisterWindowClasses(HINSTANCE hInstance)
 {
 	WNDCLASSEX wcex;
+	ZeroMemory(&wcex, sizeof(wcex));
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = 0;
+	wcex.hCursor = 0;
+	wcex.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = main_window_class_name;
+	wcex.hIconSm = 0;
+	if (!RegisterClassEx(&wcex)) {
+		return FALSE;
+	}
+
+	ZeroMemory(&wcex, sizeof(wcex));
 	wcex.cbSize = sizeof(WNDCLASSEX);
 	wcex.style = CS_HREDRAW | CS_VREDRAW;
 	wcex.lpfnWndProc = WndProc;
@@ -209,8 +321,12 @@ ATOM RegisterMainWindow(const char * name, HINSTANCE hInstance)
 	wcex.hCursor = 0;
 	wcex.hbrBackground = 0;
 	wcex.lpszMenuName = NULL;
-	wcex.lpszClassName = name;
+	wcex.lpszClassName = render_window_class_name;
 	wcex.hIconSm = 0;
-	return RegisterClassEx(&wcex);
+	if (!RegisterClassEx(&wcex)) {
+		return FALSE;
+	}
+
+	return TRUE;
 
 }
